4a461c533529b18a57d1be67b7a645a63a4e2857
yuliya.krivonos <Yuliya.Krivonos@south.rt.ru>
2024-11-19 21:48:31 +0300

    Задача CreateApp

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,14 @@
 import { defineComponent, createApp } from 'vue'
+
+const Component = defineComponent({
+  name: 'component',
+  setup() {
+    return {
+      dateTime: new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' })
+    }
+  },
+  template: '<div>Сегодня {{dateTime}}</div>'
+});
+const app = createApp(Component);
+
+app.mount('#app');
