7876c2aebfcdd510d8bb37edf27675ff49dbec7d
yuliya.krivonos <Yuliya.Krivonos@south.rt.ru>
2024-11-30 12:22:00 +0300

    Правки компоненты Карты

--- 02-basics-2/20-broken-map/MapApp.js
+++ 02-basics-2/20-broken-map/MapApp.js
@@ -5,28 +5,30 @@ export default defineComponent({
 
   setup() {
     // Реактивные переменные для хранения координат метки
-    let x = ref(0)
-    let y = ref(0)
+    const x = ref(0)
+    const y = ref(0)
 
     /**
      * Обработчик клика по карте для установки координат метки
      * @param {MouseEvent} event
      */
-    function handleClick(event) {
-      x = event.offsetX
-      y = event.offsetY
+    const handleClick = (event) => {
+      x.value = event.offsetX
+      y.value = event.offsetY
     }
 
     // Следим за X и Y для установки нового положения
     watch([x, y], () => {
       // Находим метку и изменяем её положение
       const map = document.querySelector('.pin')
-      map.style.left = `${x}px`
-      map.style.top = `${y}px`
+      map.style.left = `${x.value}px`
+      map.style.top = `${y.value}px`
     })
 
     return {
       handleClick,
+      x,
+      y
     }
   },
 
