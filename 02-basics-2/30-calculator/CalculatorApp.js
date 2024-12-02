5801ae6e4516ce59558454043c057f6d6af2e92b
javascriptru <67475495+jsru-1@users.noreply.github.com>
2024-12-02 22:27:20 +0300

    Added solution for task: Калькулятор

--- 02-basics-2/30-calculator/CalculatorApp.js
+++ 02-basics-2/30-calculator/CalculatorApp.js
@@ -1,26 +1,65 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CalculatorApp',
 
-  setup() {},
+  setup() {
+    const a = ref(0)
+    const b = ref(0)
+    const operator = ref('sum')
+    const operations = [
+      {
+        operator: 'sum',
+        label: '➕',
+      },
+      {
+        operator: 'subtract',
+        label: '➖',
+      },
+      {
+        operator: 'multiply',
+        label: '✖',
+      },
+      {
+        operator: 'divide',
+        label: '➗',
+      },
+    ]
+
+    const calculations = {
+      sum: (a, b) => a + b,
+      subtract: (a, b) => a - b,
+      multiply: (a, b) => a * b,
+      divide: (a, b) => a / b,
+    }
+
+    const result = computed(() => calculations[operator.value]?.(a.value, b.value))
+
+    return {
+      a,
+      b,
+      operator,
+      result,
+      operations,
+    }
+  },
 
   template: `
     <div class="calculator">
-      <input type="number" aria-label="First operand" />
+      <input type="number" v-model="a" aria-label="First operand" />
 
       <div class="calculator__operators">
-        <label><input type="radio" name="operator" value="sum"/>➕</label>
-        <label><input type="radio" name="operator" value="subtract"/>➖</label>
-        <label><input type="radio" name="operator" value="multiply"/>✖</label>
-        <label><input type="radio" name="operator" value="divide"/>➗</label>
+        <label v-for="operation in operations">
+          <input v-model="operator" type="radio" name="operator" :value="operation.operator"/>
+          {{ operation.label }}
+        </label>
       </div>
 
-      <input type="number" aria-label="Second operand" />
+      <input type="number" v-model="b" aria-label="Second operand" />
 
       <div>=</div>
 
-      <output>0</output>
+      <output>{{ result }}</output>
     </div>
   `,
 })
