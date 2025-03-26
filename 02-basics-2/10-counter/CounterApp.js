33094c2a4d58fe0badc826a12a23320416c7f30d
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-03-26 23:57:45 +0300

    Added solution for task: Счётчик

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,9 +1,25 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    const MIN = 0
+    const MAX = 5
+
+    const counter = ref(0)
+
+    const increment = () => (counter.value += 1)
+    const decrement = () => (counter.value -= 1)
+
+    return {
+      counter,
+      MIN,
+      MAX,
+      increment,
+      decrement,
+    }
+  },
 
   template: `
     <div class="counter">
@@ -11,15 +27,18 @@ export default defineComponent({
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        :disabled="counter <= MIN"
+        @click="decrement"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{ counter }}</span>
 
       <button
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        :disabled="counter >= MAX"
+        @click="increment"
       >➕</button>
     </div>
   `,
